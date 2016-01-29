bba5145126d2f48ada14de943536710a39f3ae0c
Raleigh Matlock <raleighman2@gmail.com>
2016-01-28 22:15:26 -0500

    AOSPB -> Turbo

--- prebuilt/common/bin/50-turbo.sh
+++ prebuilt/common/bin/50-turbo.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 # 
 # /system/addon.d/50-turbo.sh
-# During a Slim upgrade, this script backs up /system/etc/hosts,
+# During a Turbo upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
