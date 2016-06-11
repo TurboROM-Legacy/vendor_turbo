0b03fc5bee22985370098096ab71cfbb34bec3d5
Raleigh Matlock <raleighman2@gmail.com>
2016-06-11 13:15:04 -0400

    Add the Turbo versioning system
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Include the versioning makefile
+include vendor/turbo/config/versioning.mk
+
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
